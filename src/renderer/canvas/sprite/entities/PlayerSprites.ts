bd35af9f054642bf2723fc56f0d0195f6097591d
jate-koh <jate_ben@hotmail.com>
2022-09-12 23:31:37 +0700

    merge with main branch & update PlayerSprite.ts

--- src/renderer/canvas/sprite/entities/PlayerSprites.ts
+++ src/renderer/canvas/sprite/entities/PlayerSprites.ts
@@ -5,7 +5,7 @@ import { EntityState } from "@/wrapper/Entity";
 export class PlayerSprites implements EntitySprites {
 
   private readonly spriteResourceByState: Map<EntityState, SpriteResource> = new Map([
-    [EntityState.IDLE,    new SpriteResource('character/player/idle.png',   5)],
+    [EntityState.IDLE,    new SpriteResource('character/player/idle.png',   3)],
     [EntityState.ATTACK,  new SpriteResource('character/player/attack.png', 7)],
     [EntityState.HURT,    new SpriteResource('character/player/hurt.png',   3)],
   ]);
