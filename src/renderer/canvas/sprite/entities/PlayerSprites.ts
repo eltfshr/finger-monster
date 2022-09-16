579d681d5818ef9f6b22cc073b39b52858c2e73a
jate-koh <jate_ben@hotmail.com>
2022-09-17 00:01:59 +0700

    refactor(sprites): change leaf ranger and background assets

--- src/renderer/canvas/sprite/entities/PlayerSprites.ts
+++ src/renderer/canvas/sprite/entities/PlayerSprites.ts
@@ -5,7 +5,7 @@ import { EntityState } from "@/wrapper/entities/Entity";
 export class PlayerSprites implements EntitySprites {
 
   private readonly spriteResourceByState: Map<EntityState, SpriteResource> = new Map([
-    [EntityState.IDLE,    new SpriteResource('character/player/idle.png',   5, 10)],
+    [EntityState.IDLE,    new SpriteResource('character/player/idle.png',   12, 10)],
     [EntityState.ATTACK,  new SpriteResource('character/player/attack.png', 7, 10)],
     [EntityState.HURT,    new SpriteResource('character/player/hurt.png',   3, 10)],
   ]);
