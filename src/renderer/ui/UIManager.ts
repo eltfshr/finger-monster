f66e76d41b97868b70e07c4ce43c1a593dfb0ffd
riflowth <riflowth@gmail.com>
2022-09-17 23:27:57 +0700

    style: change all event related classes

--- src/renderer/ui/UIManager.ts
+++ src/renderer/ui/UIManager.ts
@@ -1,9 +1,6 @@
 export abstract class UIManager {
-  protected ui: HTMLDivElement;
 
-  public constructor() {
-    this.ui = document.querySelector<HTMLDivElement>('#ui')!;
-  }
+  protected readonly ui: HTMLDivElement = document.querySelector<HTMLDivElement>('#ui')!;
 
   public clear(): void {
     this.ui.replaceChildren();
--- src/renderer/ui/battleui/BattleHealthBar.ts
+++ src/renderer/ui/battleui/BattleHealthBar.ts
@@ -1,24 +1,22 @@
 export class BattleHealthBar {
-  private ui: HTMLDivElement;
+
+  private readonly ui: HTMLDivElement
+  private readonly healthBar: HTMLDivElement;
+
   private health: number = 100;
-  private healthBar!: HTMLDivElement;
-  private style: string = 'position: fixed; bottom: 5%; left: 2%; background-color: orangered;';
 
   public constructor(ui: HTMLDivElement) {
     this.ui = ui;
-  }
 
-  public init(): void {
     this.healthBar = document.createElement('div');
     this.healthBar.setAttribute('id', 'health-bar');
-    this.ui.appendChild(this.healthBar);
-    this.healthBar.setAttribute('style', this.style);
-    this.healthBar.style.width = `${this.ui.offsetWidth / 52}px`;
-    this.draw();
-  }
+    this.healthBar.style.width = (this.ui.offsetWidth / 52) + 'px';
 
+    this.ui.appendChild(this.healthBar);
+  }
+  
   private draw(): void {
-    this.healthBar.style.height = `${this.ui.offsetHeight / 4 * this.health / 100}px`;
+    this.healthBar.style.height = (this.ui.offsetHeight / 4 * this.health / 100) + 'px';
   }
 
   public updateHealth(health: number): void {
